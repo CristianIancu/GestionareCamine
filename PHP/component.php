4cce191b80be2a0c362ce57afe0911f173f75320
DeborahCimpan <73785320+DeborahCimpan@users.noreply.github.com>
2021-05-30 16:42:46 +0300

    component

--- PHP/component.php
+++ PHP/component.php
@@ -75,164 +75,216 @@ function component5($etaj, $loc_ocup, $loc_max){
     echo $element;
 }
 
-function componentcamera($nrcamera,$studenti,$loc_max)
+
+	function componentcamera($nrcamera,$studenti,$loc_max)
 {
-	$element = "
-	<div id=\"about\" style=\"display: none;\">
-		   <b><p style=\"text-align:center\"><h4>Număr cameră: $nrcamera</h4></p></b>
-		   <b><p style=\"text-align:center\"><h4>Studenții ce se află în cameră: $studenti</h4></p></b>
-		   <b><p style=\"text-align:center\" ><h4>Numărul de locuri maxime: $loc_max</h4></p></b>
-	
-	</div>
-	
-	
-	
-    ";
-    echo $element;
+	$names=[];
+	$pre=[];
+	foreach($studenti as $value){
+		array_push($names,$value["Nume"]);
+		array_push($pre,$value["Prenume"]);
+	}
+	$element="<div id=\"about\" style=\"display: none;\">
+	<b><p style=\"text-align:center\"><h4>Număr cameră: $nrcamera</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>Studenții ce se află în cameră: </h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[0] $pre[0]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[1] $pre[1]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[2] $pre[2]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[3] $pre[3]</h4></p></b>
+	<b><p style=\"text-align:center\" ><h4>Numărul de locuri maxime: $loc_max</h4></p></b>
+	</div>";
+
+  echo $element;
 }
+
 
 function componentcamera2($nrcamera,$studenti,$loc_max)
 {
-	$element = "
-	<div id=\"about2\" style=\"display: none;\">
-		   <b><p style=\"text-align:center\"><h4>Număr cameră: $nrcamera</h4></p></b>
-		   <b><p style=\"text-align:center\"><h4>Studenții ce se află în cameră: $studenti</h4></p></b>
-		   <b><p style=\"text-align:center\" ><h4>Numărul de locuri maxime: $loc_max</h4></p></b>
-	
-	</div>
-	
-	
-	
-    ";
-    echo $element;
+$names=[];
+	$pre=[];
+	foreach($studenti as $value){
+		array_push($names,$value["Nume"]);
+		array_push($pre,$value["Prenume"]);
+	}
+	$element="<div id=\"about2\" style=\"display: none;\">
+	<b><p style=\"text-align:center\"><h4>Număr cameră: $nrcamera</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>Studenții ce se află în cameră: </h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[0] $pre[0]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[1] $pre[1]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[2] $pre[2]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[3] $pre[3]</h4></p></b>
+	<b><p style=\"text-align:center\" ><h4>Numărul de locuri maxime: $loc_max</h4></p></b>
+	</div>";
+
+  echo $element;
 }
 
 function componentcamera3($nrcamera,$studenti,$loc_max)
 {
-	$element = "
-	<div id=\"about3\" style=\"display: none;\">
-		   <b><p style=\"text-align:center\"><h4>Număr cameră: $nrcamera</h4></p></b>
-		   <b><p style=\"text-align:center\"><h4>Studenții ce se află în cameră: $studenti</h4></p></b>
-		   <b><p style=\"text-align:center\" ><h4>Numărul de locuri maxime: $loc_max</h4></p></b>
-	
-	</div>
-	
-	
-	
-    ";
-    echo $element;
+	$names=[];
+	$pre=[];
+	foreach($studenti as $value){
+		array_push($names,$value["Nume"]);
+		array_push($pre,$value["Prenume"]);
+	}
+	$element="<div id=\"about3\" style=\"display: none;\">
+	<b><p style=\"text-align:center\"><h4>Număr cameră: $nrcamera</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>Studenții ce se află în cameră: </h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[0] $pre[0]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[1] $pre[1]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[2] $pre[2]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[3] $pre[3]</h4></p></b>
+	<b><p style=\"text-align:center\" ><h4>Numărul de locuri maxime: $loc_max</h4></p></b>
+	</div>";
+
+  echo $element;
 }
 
 function componentcamera4($nrcamera,$studenti,$loc_max)
 {
-	$element = "
-	<div id=\"about4\" style=\"display: none;\">
-		   <b><p style=\"text-align:center\"><h4>Număr cameră: $nrcamera</h4></p></b>
-		   <b><p style=\"text-align:center\"><h4>Studenții ce se află în cameră: $studenti</h4></p></b>
-		   <b><p style=\"text-align:center\" ><h4>Numărul de locuri maxime: $loc_max</h4></p></b>
-	
-	</div>
-	
-	
-	
-    ";
-    echo $element;
+	$names=[];
+	$pre=[];
+	foreach($studenti as $value){
+		array_push($names,$value["Nume"]);
+		array_push($pre,$value["Prenume"]);
+	}
+	$element="<div id=\"about4\" style=\"display: none;\">
+	<b><p style=\"text-align:center\"><h4>Număr cameră: $nrcamera</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>Studenții ce se află în cameră: </h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[0] $pre[0]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[1] $pre[1]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[2] $pre[2]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[3] $pre[3]</h4></p></b>
+	<b><p style=\"text-align:center\" ><h4>Numărul de locuri maxime: $loc_max</h4></p></b>
+	</div>";
+
+  echo $element;
 }
 
 function componentcamera5($nrcamera,$studenti,$loc_max)
 {
-	$element = "
-	<div id=\"about5\" style=\"display: none;\">
-		   <b><p style=\"text-align:center\"><h4>Număr cameră: $nrcamera</h4></p></b>
-		   <b><p style=\"text-align:center\"><h4>Studenții ce se află în cameră: $studenti</h4></p></b>
-		   <b><p style=\"text-align:center\" ><h4>Numărul de locuri maxime: $loc_max</h4></p></b>
-	
-	</div>
-	
-	
-	
-    ";
-    echo $element;
+	$names=[];
+	$pre=[];
+	foreach($studenti as $value){
+		array_push($names,$value["Nume"]);
+		array_push($pre,$value["Prenume"]);
+	}
+	$element="<div id=\"about5\" style=\"display: none;\">
+	<b><p style=\"text-align:center\"><h4>Număr cameră: $nrcamera</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>Studenții ce se află în cameră: </h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[0] $pre[0]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[1] $pre[1]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[2] $pre[2]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[3] $pre[3]</h4></p></b>
+	<b><p style=\"text-align:center\" ><h4>Numărul de locuri maxime: $loc_max</h4></p></b>
+	</div>";
+
+  echo $element;
 }
 
 function componentcamera6($nrcamera,$studenti,$loc_max)
 {
-	$element = "
-	<div id=\"about6\" style=\"display: none;\">
-		   <b><p style=\"text-align:center\"><h4>Număr cameră: $nrcamera</h4></p></b>
-		   <b><p style=\"text-align:center\"><h4>Studenții ce se află în cameră: $studenti</h4></p></b>
-		   <b><p style=\"text-align:center\" ><h4>Numărul de locuri maxime: $loc_max</h4></p></b>
-	
-	</div>
-	
-	
-	
-    ";
-    echo $element;
+	$names=[];
+	$pre=[];
+	foreach($studenti as $value){
+		array_push($names,$value["Nume"]);
+		array_push($pre,$value["Prenume"]);
+	}
+	$element="<div id=\"about6\" style=\"display: none;\">
+	<b><p style=\"text-align:center\"><h4>Număr cameră: $nrcamera</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>Studenții ce se află în cameră: </h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[0] $pre[0]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[1] $pre[1]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[2] $pre[2]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[3] $pre[3]</h4></p></b>
+	<b><p style=\"text-align:center\" ><h4>Numărul de locuri maxime: $loc_max</h4></p></b>
+	</div>";
+
+  echo $element;
 }
 
 function componentcamera7($nrcamera,$studenti,$loc_max)
 {
-	$element = "
-	<div id=\"about7\" style=\"display: none;\">
-		   <b><p style=\"text-align:center\"><h4>Număr cameră: $nrcamera</h4></p></b>
-		   <b><p style=\"text-align:center\"><h4>Studenții ce se află în cameră: $studenti</h4></p></b>
-		   <b><p style=\"text-align:center\" ><h4>Numărul de locuri maxime: $loc_max</h4></p></b>
-	
-	</div>
-	
-	
-	
-    ";
-    echo $element;
+	$names=[];
+	$pre=[];
+	foreach($studenti as $value){
+		array_push($names,$value["Nume"]);
+		array_push($pre,$value["Prenume"]);
+	}
+	$element="<div id=\"about7\" style=\"display: none;\">
+	<b><p style=\"text-align:center\"><h4>Număr cameră: $nrcamera</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>Studenții ce se află în cameră: </h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[0] $pre[0]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[1] $pre[1]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[2] $pre[2]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[3] $pre[3]</h4></p></b>
+	<b><p style=\"text-align:center\" ><h4>Numărul de locuri maxime: $loc_max</h4></p></b>
+	</div>";
+
+  echo $element;
 }
 
 function componentcamera8($nrcamera,$studenti,$loc_max)
 {
-	$element = "
-	<div id=\"about8\" style=\"display: none;\">
-		   <b><p style=\"text-align:center\"><h4>Număr cameră: $nrcamera</h4></p></b>
-		   <b><p style=\"text-align:center\"><h4>Studenții ce se află în cameră: $studenti</h4></p></b>
-		   <b><p style=\"text-align:center\" ><h4>Numărul de locuri maxime: $loc_max</h4></p></b>
-	
-	</div>
-	
-	
-	
-    ";
-    echo $element;
+	$names=[];
+	$pre=[];
+	foreach($studenti as $value){
+		array_push($names,$value["Nume"]);
+		array_push($pre,$value["Prenume"]);
+	}
+	$element="<div id=\"about8\" style=\"display: none;\">
+	<b><p style=\"text-align:center\"><h4>Număr cameră: $nrcamera</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>Studenții ce se află în cameră: </h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[0] $pre[0]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[1] $pre[1]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[2] $pre[2]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[3] $pre[3]</h4></p></b>
+	<b><p style=\"text-align:center\" ><h4>Numărul de locuri maxime: $loc_max</h4></p></b>
+	</div>";
+
+  echo $element;
 }
 
 function componentcamera9($nrcamera,$studenti,$loc_max)
 {
-	$element = "
-	<div id=\"about9\" style=\"display: none;\">
-		   <b><p style=\"text-align:center\"><h4>Număr cameră: $nrcamera</h4></p></b>
-		   <b><p style=\"text-align:center\"><h4>Studenții ce se află în cameră: $studenti</h4></p></b>
-		   <b><p style=\"text-align:center\" ><h4>Numărul de locuri maxime: $loc_max</h4></p></b>
-	
-	</div>
-	
-	
-	
-    ";
-    echo $element;
+	$names=[];
+	$pre=[];
+	foreach($studenti as $value){
+		array_push($names,$value["Nume"]);
+		array_push($pre,$value["Prenume"]);
+	}
+	$element="<div id=\"about9\" style=\"display: none;\">
+	<b><p style=\"text-align:center\"><h4>Număr cameră: $nrcamera</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>Studenții ce se află în cameră: </h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[0] $pre[0]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[1] $pre[1]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[2] $pre[2]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[3] $pre[3]</h4></p></b>
+	<b><p style=\"text-align:center\" ><h4>Numărul de locuri maxime: $loc_max</h4></p></b>
+	</div>";
+
+  echo $element;
 }
 
 function componentcamera10($nrcamera,$studenti,$loc_max)
 {
-	$element = "
-	<div id=\"about10\" style=\"display: none;\">
-		   <b><p style=\"text-align:center\"><h4>Număr cameră: $nrcamera</h4></p></b>
-		   <b><p style=\"text-align:center\"><h4>Studenții ce se află în cameră: $studenti</h4></p></b>
-		   <b><p style=\"text-align:center\" ><h4>Numărul de locuri maxime: $loc_max</h4></p></b>
-	
-	</div>
-	
-	
-	
-    ";
-    echo $element;
+	$names=[];
+	$pre=[];
+	foreach($studenti as $value){
+		array_push($names,$value["Nume"]);
+		array_push($pre,$value["Prenume"]);
+	}
+	$element="<div id=\"about10\" style=\"display: none;\">
+	<b><p style=\"text-align:center\"><h4>Număr cameră: $nrcamera</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>Studenții ce se află în cameră: </h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[0] $pre[0]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[1] $pre[1]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[2] $pre[2]</h4></p></b>
+	<b><p style=\"text-align:center\"><h4>$names[3] $pre[3]</h4></p></b>
+	<b><p style=\"text-align:center\" ><h4>Numărul de locuri maxime: $loc_max</h4></p></b>
+	</div>";
+
+  echo $element;
 }
 
 
